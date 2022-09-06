d6e8b60e74337eaf1d158b57bb09ba90c4fed787
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-06 15:38:02 -0400

    working on setting up actions

---
+++ dad-better-frontend/src/actions/quoteActions.js
@@ -0,0 +1,8 @@
+export const fetchQuotes = () => {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_QUOTES'});
+        fetch("http://localhost:3000/quotes")
+        .then ((response) => {return response.json()})
+        .then((responseJSON) => {dispatch({type: "ADD_QUOTES", users: responseJSON})})
+    }
+}
