94d8ea5610c54d486a7a85b044ff70f63b4400bd
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-06 14:01:47 -0400

    wrapped <App> in Provider

--- dad-better-frontend/src/index.js
+++ dad-better-frontend/src/index.js
@@ -12,9 +12,10 @@ const store = createStore(usersReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
