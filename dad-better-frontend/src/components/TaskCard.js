73605d83b2d806856c415bc805fb6e91162892b6
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-06 15:37:43 -0400

    setting up initial task displays

--- dad-better-frontend/src/components/TaskCard.js
+++ dad-better-frontend/src/components/TaskCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const TaskCard = (props) => {
-    <div>
-        TaskCard
-    </div>
+    return (
+        <div>
+            TaskCard
+        </div>
+    )
 }
 
 export default TaskCard
--- dad-better-frontend/src/containers/TaskContainer.js
+++ dad-better-frontend/src/containers/TaskContainer.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import TaskCard from '../components/TaskCard';
 
 class TaskContainer extends Component {
     render(){
         return (
             <div>
-                Task Container
+                <TaskCard />
             </div>
         )
     }
