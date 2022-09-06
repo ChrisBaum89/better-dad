0f5f4787fed8a68bbf59f9b7d1aab41af58657a4
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-05 20:51:08 -0400

    creating initial User seeds to start testing

--- dad-better-backend/db/seeds.rb
+++ dad-better-backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(
+    username: "ChrisBaum89",
+    name: "Chris",
+    email: "c@gmail.com"
+)
+
+user2 = User.create(
+    username: "BritB90",
+    name: "Brit",
+    email: "b@gmail.com"
+)
