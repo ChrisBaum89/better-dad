16aa57cb5e951ffd44e8243b0bf67061e40ca63f
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-07 15:31:09 -0400

    turned into function and modified form

--- dad-better-frontend/src/components/LoginForm.js
+++ dad-better-frontend/src/components/LoginForm.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
-class LoginForm extends Component {
-    render() {
-        return (
-            <div>
-                <Form>
-                    <Form.Group className="w-25" controlId="formBasicEmail">
-                        <Form.Control type="username" size="sm" placeholder="Username" />
-                    </Form.Group>
-                    <Form.Group className="w-25" controlId="formBasicPassword">
-                        <Form.Control type="password" size="sm" htmlSize="10" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Login
-                    </Button>
-                    <Button variant="primary" type="submit">
-                        Signup
-                    </Button>
-                </Form>
-            </div>
-        )
+function LoginForm(props) {
+    
+    const handleOnSubmit = (event) => {
+        debugger
+        event.preventDefault()
     }
+    
+    return (
+        <div>
+            <Form onSubmit={handleOnSubmit}>
+                <Form.Group className="w-25" controlId="formBasicEmail">
+                    <Form.Control type="username" size="sm" placeholder="Username" />
+                </Form.Group>
+                <Form.Group className="w-25" controlId="formBasicPassword">
+                    <Form.Control type="password" size="sm" htmlSize="10" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+                <Button variant="primary" onClick={props.handleShow}>
+                    Signup
+                </Button>
+            </Form>
+        </div>
+    )
+
 }
+
+
 
 export default LoginForm
