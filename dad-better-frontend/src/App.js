933592b9ad784e224e520f52fa36e5f63abc89e5
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-07 15:30:05 -0400

    changed class into function

--- dad-better-frontend/src/App.js
+++ dad-better-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import logo from './logo.svg';
 import './css/App.css';
 import { connect } from "react-redux";
@@ -12,31 +12,48 @@ import TaskContainer from './containers/TaskContainer';
 import NavigationContainer from './containers/NavigationContainer';
 import Image from 'react-bootstrap/Image'
 import MyImage from './img/better-dad-logo.png'
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Signup from './components/Signup';
+import Button from 'react-bootstrap/Button';
 
-class App extends Component {
+function App(){
 
-  componentDidMount() {
-    this.props.fetchUsers()
-    this.props.fetchQuotes()
-  }
+  // componentDidMount() {
+  //   this.props.fetchUsers()
+  //   this.props.fetchQuotes()
+  // }
+    
+  const [show, setShow] = useState(false);
 
-  render() {
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
       <div>
           <div className="better-dad-logo">
             <Image src={MyImage} alt="Better Dad" display="inline-block"></Image>
           </div>
           <div className="login-container">
-            <LoginContainer />
+            <LoginContainer handleShow={handleShow}/>
           </div>
+      
           <UserContainer />
           <BadgeContainer />
           <QuoteContainer />
           <TaskContainer />
           <NavigationContainer />
+          <Button variant="primary" onClick={handleShow}>Launch</Button>
+          <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Signup</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Signup />
+            </Offcanvas.Body>
+          </Offcanvas>
       </div>
     )
-  }
+  
 }
 
 const mapStateToProps = (state) => {
