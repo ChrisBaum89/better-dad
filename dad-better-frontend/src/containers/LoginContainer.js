8f73aa923859747d03fb9aeb52551f8b0eeab410
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-07 15:30:43 -0400

    passed in handleShow props to LoginForm

--- dad-better-frontend/src/containers/LoginContainer.js
+++ dad-better-frontend/src/containers/LoginContainer.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import LoginForm from '../components/LoginForm';
 
 class LoginContainer extends Component {
+    
     render(){
         return (
             <div>
-                <LoginForm />
+                <LoginForm handleShow={this.props.handleShow}/>
             </div>
         )
     }
