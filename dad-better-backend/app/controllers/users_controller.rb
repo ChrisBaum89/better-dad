115227296f5c1192613c025666aa8e1df1fdc47d
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-07 14:31:15 -0400

    working on signup/login logic

--- dad-better-backend/app/controllers/users_controller.rb
+++ dad-better-backend/app/controllers/users_controller.rb
@@ -1,27 +1,17 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
-    # def index
-    #     users = User.all
-    #     options = {include: [:children, :badges, :tasks]}
-    #     render json: UserSerializer.new(users, options)
-    # end
 
-    # def show
-    #     user = User.find_by(id: params[:id])
-    #     options = {
-    #         include: [:children, :badges, :tasks]
-    #     }
-    #     render json: UserSerializer.new(user, options)
-    # end
 
     def create
-        @user = User.new(user_params)
-        @user.score = 0
-        @user.level = 0
-        binding.pry
+        @user = User.create(user_params)
         if @user.valid?
-            @user.save
-            render json: { user: UserSerializer.new(@user) }, status: :created
+          @token = encode_token(user_id: @user.id)
+          render json: {
+                   user: UserSerializer.new(@user),
+                   jwt: @token,
+                 },
+                 status: :created
         else
           render json: {
                    error: 'failed to create user',
@@ -36,3 +26,17 @@ class UsersController < ApplicationController
         params.permit(:username, :password, :email, :name)
       end
 end
+
+    # def index
+    #     users = User.all
+    #     options = {include: [:children, :badges, :tasks]}
+    #     render json: UserSerializer.new(users, options)
+    # end
+
+    # def show
+    #     user = User.find_by(id: params[:id])
+    #     options = {
+    #         include: [:children, :badges, :tasks]
+    #     }
+    #     render json: UserSerializer.new(user, options)
+    # end
