a3b4fd0782708f2e1309d9e1e444880d236853df
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-06 21:36:57 -0400

    created task card using card element

--- dad-better-frontend/src/components/TaskCard.js
+++ dad-better-frontend/src/components/TaskCard.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
 
 const TaskCard = (props) => {
     return (
-        <div>
-            TaskCard
+        <div class="col d-flex justify-content-center">
+            <Card style={{ width: '18rem' }} >
+                <Card.Body>
+                    <Card.Text>
+                        TaskCard
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
