b73e5d06697019f4721cd1d31ca17d4d96aaaec1
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-07 12:41:36 -0400

    added create method

--- dad-better-backend/app/controllers/users_controller.rb
+++ dad-better-backend/app/controllers/users_controller.rb
@@ -13,4 +13,16 @@ class UsersController < ApplicationController
         }
         render json: UserSerializer.new(user, options)
     end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: {user: UserSerializer.new(@user)}, status :created
+        else
+            render json: {
+                error: 'failed to create user'
+            },
+            status: :unprocessable_entity
+        end
+    end
 end
