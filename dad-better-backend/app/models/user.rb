03f72f3016a0d12702fc6e4140a3502fda0ca28c
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-07 10:27:28 -0400

    realignment of join tables

--- dad-better-backend/app/models/user.rb
+++ dad-better-backend/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
     has_many :children
-    has_many :achievements
-    has_many :completedtasks
-    has_many :favorites
+    has_many :assigned_tasks
+    has_many :completed_tasks
+    has_many :earned_badges
     has_many :tasks, through: :assigned_tasks
-    has_many :badges, through: :achievements
+    has_many :tasks, through: :completed_tasks
+    has_many :badges, through: :earned_badges
 
 end
--- dad-better-backend/db/migrate/20220907141729_create_users_badges.rb
+++ dad-better-backend/db/migrate/20220907141729_create_users_badges.rb
@@ -1,6 +1,6 @@
 class CreateUsersBadges < ActiveRecord::Migration[6.1]
   def change
-    create_table :user_badge do |t|
+    create_table :earned_badges do |t|
       t.integer :user_id
       t.integer :badge_id
 
