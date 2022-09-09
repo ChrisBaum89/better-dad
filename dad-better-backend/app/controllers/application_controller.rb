16bf5ad1ae35ad63bb5f0d2ac09f6878312facee
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-09 11:42:25 -0400

    resovled formatting issue

--- dad-better-backend/app/controllers/application_controller.rb
+++ dad-better-backend/app/controllers/application_controller.rb
@@ -35,8 +35,7 @@ class ApplicationController < ActionController::API
   end
 
   def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized 
-    unless logged_in?
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
     end
   end
 end
