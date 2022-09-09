998f2cbd075e7cfbc1863411f6d8c39c917ad7ce
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-09 11:41:50 -0400

    resolved indent/end issues

--- dad-better-backend/app/controllers/application_controller.rb
+++ dad-better-backend/app/controllers/application_controller.rb
@@ -1,44 +1,42 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
-    
-    def encode_token(payload)
-        JWT.encode(payload, 'my_s3cr3t')
-    end
+  before_action :authorized
+  def encode_token(payload)
+    JWT.encode(payload, 'my_s3cr3t')
+  end
 
-    def auth_header
-        # { 'Authorization': 'Bearer <token>' }
-        request.headers['Authorization']
-    end
-    
-    def decoded_token
-      if auth_header
-        token = auth_header.split(' ')[1]
-          # headers: { 'Authorization': 'Bearer <token>' }
-        begin
-          JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
-            # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
-        rescue JWT::DecodeError
-            nil
-        end
+  def auth_header
+    # { 'Authorization': 'Bearer <token>' }
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      # headers: { 'Authorization': 'Bearer <token>' }
+      begin
+        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+      # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+      rescue JWT::DecodeError
+        nil
       end
     end
+  end
 
-    def current_user
-      if decoded_token
-        # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
-        # or nil if we can't decode the token
-        user_id = decoded_token[0]['user_id']
-        @user = User.find_by(id: user_id)
-      end
+  def current_user
+    if decoded_token
+      # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
     end
-        
-    def logged_in?
-      !!current_user
-    end
+  end
 
-    def authorized
-      unless logged_in?
-        render json: { message: 'Please log in' }, status: :unauthorized
-      end
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized 
+    unless logged_in?
     end
+  end
 end
