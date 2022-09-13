c7d2679cbf18c178df77f230ad27604d02a4d8af
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-13 15:52:44 -0400

    changed to a function and removed uncessary code

--- dad-better-frontend/src/containers/UserContainer.js
+++ dad-better-frontend/src/containers/UserContainer.js
@@ -1,22 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserCard from '../components/UserCard';
-import {connect} from "react-redux"
 
-class UserContainer extends Component {
-    render() {
+function UserContainer () {
         return (
             <div>
                 <UserCard />
             </div>
         )
-    }
 }
 
-const mapStateToProps = (state) => {
-    return {
-      user: state.usersReducer.user,
-      quote: state.quotesReducer.quotes.data
-    }
-  }
-
-export default connect(mapStateToProps)(UserContainer)
+export default UserContainer
