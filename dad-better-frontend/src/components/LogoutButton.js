ca9e2c2341b79aa9878742c711f30fa8c2819e26
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-13 15:36:58 -0400

    added LogoutButton

---
+++ dad-better-frontend/src/components/LogoutButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from 'react-bootstrap/Button'
+
+function LogoutButton() {
+    return (
+        <Button variant="primary" type="submit">
+            Logout
+        </Button>
+    )
+}
+export default LogoutButton
