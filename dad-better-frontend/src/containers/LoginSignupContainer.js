ea65ee7ec33f99f3789a6ad6d1e753e66844fd43
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-13 16:24:23 -0400

    changed LoginSignupContainer

--- dad-better-frontend/src/containers/LoginSignupContainer.js
+++ dad-better-frontend/src/containers/LoginSignupContainer.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LoginSignupForm from '../components/LoginSignupForm';
+import Signup from '../components/Signup';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function LoginSignupContainer(props) {
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
     return (
         <div>
-            <LoginSignupForm/>
+            <LoginSignupForm handleShow={handleShow}/>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Signup</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Signup />
+                </Offcanvas.Body>
+            </Offcanvas>
         </div>
     )
 
