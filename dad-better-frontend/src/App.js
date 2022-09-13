fc2a901ecb43b3893ffc8aa4b769d204b1e59982
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-13 15:46:36 -0400

    changed app to use WelcomeContainer

--- dad-better-frontend/src/App.js
+++ dad-better-frontend/src/App.js
@@ -11,13 +11,10 @@ import MyImage from './img/better-dad-logo.png'
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Signup from './components/Signup';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Welcome from "./components/Welcome"
+import WelcomeContainer from './containers/WelcomeContainer';
 
 function App() {
-  const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   return (
     <Router>
@@ -25,32 +22,14 @@ function App() {
         <div className="better-dad-logo">
           <Image src={MyImage} alt="Better Dad" display="inline-block"></Image>
         </div>
-        <div className="login-container">
-          <Route exact path="/" render={() => <LoginContainer handleShow={handleShow} />} />
-        </div>
 
-        <Route exact path="/" component={Welcome} />
+        <Route exact path="/" component={WelcomeContainer} />
         <Route exact path="/profile" component={ProfileContainer} />
 
-        <Offcanvas show={show} onHide={handleClose}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Signup</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Signup />
-          </Offcanvas.Body>
-        </Offcanvas>
       </div>
     </Router>
   )
 
 }
 
-const mapStateToProps = (state) => {
-  return {
-    users: state.usersReducer.users,
-    quote: state.quotesReducer.quotes.data
-  }
-}
-
-export default connect(mapStateToProps, { fetchUsers, fetchQuotes })(App);
+export default App;
