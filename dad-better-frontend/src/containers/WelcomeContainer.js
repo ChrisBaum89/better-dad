c700d43ab98b6aedb1922686039be5be5983db73
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-13 15:38:38 -0400

    created WelcomeContainer

---
+++ dad-better-frontend/src/containers/WelcomeContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class WelcomeContainer extends Component {
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default WelcomeContainer
