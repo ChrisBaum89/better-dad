c92c891da6147fd4657f8be535617cbd767e377f
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-13 15:46:54 -0400

    created WelcomeContainer

--- dad-better-frontend/src/containers/WelcomeContainer.js
+++ dad-better-frontend/src/containers/WelcomeContainer.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import LoginContainer from './LoginContainer';
+import Welcome from '../components/Welcome';
+import Signup from '../components/Signup';
 
 
-class WelcomeContainer extends Component {
-
-    render() {
+function WelcomeContainer () {
+    const [show, setShow] = useState(false);
+        
+    const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+        
         return (
             <div>
-            
+                <LoginContainer handleShow={handleShow} />
+                <Welcome />
+
+                <Offcanvas show={show} onHide={handleClose}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Signup</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Signup />
+                    </Offcanvas.Body>
+                </Offcanvas>
             </div>
         )
-    }
 }
 
 export default WelcomeContainer
