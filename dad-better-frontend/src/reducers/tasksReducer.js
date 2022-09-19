d009e3abf4449ecda942786b8f1aa86722cd5e7c
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-19 11:49:27 -0400

    removed loading since asynch fetch not required

--- dad-better-frontend/src/reducers/tasksReducer.js
+++ dad-better-frontend/src/reducers/tasksReducer.js
@@ -1,10 +1,5 @@
 const tasksReducer = (state = { tasks: [], loading: false }, action) => {
     switch (action.type) {
-        case "LOADING_TASKS":
-            return {
-                ...state, tasks: [...state.tasks],
-                loading: true,
-            };
         case "ADD_TASKS":
             return {
                 ...state,
