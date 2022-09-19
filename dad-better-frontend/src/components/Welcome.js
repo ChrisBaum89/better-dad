464e84cf035de6bb4e69616bf644dd55f3ed1334
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-19 08:55:08 -0400

    typo fixed

--- dad-better-frontend/src/components/Welcome.js
+++ dad-better-frontend/src/components/Welcome.js
@@ -4,7 +4,7 @@ function Welcome(){
     return (
         <div>
         <h3>Welcome to Better Dad</h3>
-        <p> This application is to help dad's become betting at "dadding" in a unique and fun way! If you are a returning user, please login.  If not please sign-up and let the fun begin!</p>
+        <p> This application is to help dad's become better at "dadding" in a unique and fun way! If you are a returning user, please login.  If not please sign-up and let the fun begin!</p>
         </div>
         )
 }
