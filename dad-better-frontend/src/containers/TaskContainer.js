6de62193e861c3fa77bd9c7e5742aa6fd503961f
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-19 10:40:24 -0400

    working on displaying assigned tasks

--- dad-better-frontend/src/containers/TaskContainer.js
+++ dad-better-frontend/src/containers/TaskContainer.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import TaskList from '../components/TaskList';
+import { useSelector, useDispatch } from 'react-redux';
 
-function TaskContainer (){
-        return (
+function TaskContainer (props){
+    const currentState = useSelector((state) => state)
+    const dispatch = useDispatch()
+    debugger
+    return (
             <div>
                 <TaskList />
             </div>
