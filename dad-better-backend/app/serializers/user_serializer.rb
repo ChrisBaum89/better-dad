8f3cc59d7cae5c7716bab734f94a5ae1f519b766
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-19 10:40:04 -0400

    updated to split tasks into completed and assigned

--- dad-better-backend/app/serializers/user_serializer.rb
+++ dad-better-backend/app/serializers/user_serializer.rb
@@ -3,6 +3,6 @@ class UserSerializer
   attributes :username, :name, :email, :score
   has_many :children
   has_many :badges, through: :earned_badges
-  has_many :tasks, through: :assigned_tasks
-  has_many :tasks, through: :completed_tasks
+  has_many :assigned_tasks, through: :assigned_tasks
+  has_many :completed_tasks, through: :completed_tasks
 end
