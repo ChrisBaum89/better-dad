0f239472e02b552d2616ab614e6edb54ea7920f3
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-19 08:55:20 -0400

    added fetchTasks

---
+++ dad-better-frontend/src/actions/taskActions.js
@@ -0,0 +1,8 @@
+export const fetchTasks = () => {
+    return (dispatch) => {
+        dispatch({ type: 'LOADING_USERS' });
+        fetch("http://localhost:3000/tasks")
+            .then((response) => { return response.json() })
+            .then((responseJSON) => { dispatch({ type: "ADD_TASK", tasks: responseJSON })})
+    }
+}
