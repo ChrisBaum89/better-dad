0f0ed26b5581a2d8715702b30b874869d5a8cff0
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-10 14:37:20 -0500

    removed unneeded tasks

--- dad-better-frontend/src/reducers/tasksReducer.js
+++ dad-better-frontend/src/reducers/tasksReducer.js
@@ -1,18 +1,5 @@
 const tasksReducer = (state = { tasks: [], requesting: false }, action) => {
     switch (action.type) {
-        case "START_ADDING_TASKS":
-            return {
-                ...state,
-                tasks: [...state.tasks],
-                requesting: true,
-            }
-
-        case "ADD_TASKS": 
-        return {
-                ...state,
-                tasks: action.tasks,
-                requesting: true,
-            }
 
         default:
             return state
