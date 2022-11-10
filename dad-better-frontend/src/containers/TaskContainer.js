10fa2ac13115d59ebc29efdc0cb9c9f4d1ca84bd
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-10 14:33:00 -0500

    removed commented out code

--- dad-better-frontend/src/containers/TaskContainer.js
+++ dad-better-frontend/src/containers/TaskContainer.js
@@ -5,42 +5,7 @@ import { fetchTasks } from '../actions/taskActions';
 
 function TaskContainer(props) {
 
-    //const allTasks = useSelector((state) => state.tasksReducer.tasks)
     const dispatch = useDispatch()
-
-    // useEffect(() => {
-    //     fetchTasks().then((data) => {
-    //         dispatch({ type: "ADD_TASKS", tasks: data.data })
-    //     })
-    // }, [])
-
-    // const fetchTasks = async () => {
-    //     const response = await fetch("http://localhost:3000/tasks");
-    //     const tasks = await response.json();
-    //     return tasks;
-    // };
-
-    // const getAssignedTaskIds = (assignedTasks) => {
-    //     return assignedTasks.map(task => task.attributes.task_id)
-    // }
-
-    // const getTaskInfo = (allTasks, assignIds) => {
-    //     const foundTasks = []
-    //     for (let i = 0; i < allTasks.length; i++) {
-    //         if (assignIds.includes(parseInt(allTasks[i].id))) {
-    //             foundTasks.push(allTasks[i])
-    //         }
-    //     }
-    //     return foundTasks
-    // }
-
-    // const getAssignedTaskData = (assignedTasks, allTasks) => {
-    //     return getTaskInfo(allTasks, getAssignedTaskIds(assignedTasks))
-    // }
-
-    
-
-    // const userTasks = getAssignedTaskData(assignedTasks, allTasks)
 
     return (
         <div>
