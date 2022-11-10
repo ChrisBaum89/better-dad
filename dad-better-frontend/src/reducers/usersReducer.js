64113404ffd568246deba3eb605a51d7a8247ec4
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-10 14:35:22 -0500

    removed UPDATE_SCORE

--- dad-better-frontend/src/reducers/usersReducer.js
+++ dad-better-frontend/src/reducers/usersReducer.js
@@ -4,11 +4,6 @@ const usersReducer = (state = { user: [], loading: false }, action) => {
         case "SET_USER":
             return { ...state, user: [action.payload.user] }
 
-        case "UPDATE_SCORE":
-            const updatedUser = state
-            updatedUser.user[0].data.attributes.score = action.payload.user.data.attributes.score
-            return { ...state, updatedUser}
-
         case "UPDATE_USER":
             return {...state, user: [action.payload.user]}
 
