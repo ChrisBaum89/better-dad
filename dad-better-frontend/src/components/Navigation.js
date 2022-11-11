1c84f241bfca8e5350281c5d64c86a2170b8468d
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-11 12:23:30 -0500

    converted to hook

--- dad-better-frontend/src/components/Navigation.js
+++ dad-better-frontend/src/components/Navigation.js
@@ -3,18 +3,17 @@ import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
-class Navigation extends Component {
-    render() {
-        return (
-            <div>
-                <ButtonToolbar aria-label="Toolbar with button groups">
-                    <ButtonGroup className="me-2" aria-label="First group">
-                        <Button>History</Button> <Button>Favorites</Button>
-                    </ButtonGroup>
-                </ButtonToolbar>
-            </div>
-        )
-    }
+const Navigation = () => {
+    return (
+        <div>
+            <ButtonToolbar aria-label="Toolbar with button groups">
+                <ButtonGroup className="me-2" aria-label="First group">
+                    <Button>History</Button>
+                    <Button>Favorites</Button>
+                </ButtonGroup>
+            </ButtonToolbar>
+        </div>
+    )
 }
 
 export default Navigation
