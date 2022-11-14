43ab0305c38ed15068746e4ac6648c56562fa147
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-14 14:13:02 -0500

    updating the user update based on update type

--- dad-better-backend/app/controllers/users_controller.rb
+++ dad-better-backend/app/controllers/users_controller.rb
@@ -23,9 +23,9 @@ class UsersController < ApplicationController
 
   def render_error_json
     render json: {
-      error: 'failed to create user'
-    },
-    status: :unprocessable_entity
+             error: 'failed to create user'
+           },
+           status: :unprocessable_entity
   end
 
   def show
@@ -39,14 +39,17 @@ class UsersController < ApplicationController
 
   def update_user
     @user = User.find_by_id(params[:user][:user_id])
-    update_score(@user)
-    update_task(@user)
-    update_badge(@user)
+    @task = Task.find_by_id(params[:user][:task_id])
+    @update_type = params[:user][:update_type]
+    if @update_type == 'task_completed'
+      update_badge(@user)
+      mark_task_completed(@user)
+    end
     token = params[:jwt]
     @user.save
 
     options = {
-      include: [:assigned_tasks, :completed_tasks, :earned_badges]
+      include: %i[assigned_tasks completed_tasks earned_badges]
     }
 
     render json: {
@@ -55,25 +58,25 @@ class UsersController < ApplicationController
     }
   end
 
-  def update_score(user)
-    user.score = user.score + (params[:user][:score]).to_i
-  end
+  # def update_score(user)
+  #   user.score = user.score + (params[:user][:score]).to_i
+  # end
 
-  def update_task(user)
+  def mark_task_completed(user)
     @completed_task = Task.find_by_id(params[:user][:task_id])
     CompletedTask.create(user_id: user.id, task_id: @completed_task.id)
 
     # remove @completed_task from assigned tasks
     user.assigned_tasks.delete_by(task_id: @completed_task.id)
+    user.calc_score
   end
 
   def update_badge(user)
-    applicable_badges = Badge.all.select{|badge| user.score >= badge.score_threshold}
+    applicable_badges = Badge.all.select { |badge| user.score >= badge.score_threshold }
     applicable_badges.each do |badge|
       if user.badges.include?(badge)
-      
-     elsif 
-        EarnedBadge.create(user_id: user.id, badge_id: badge.id)
+
+      elsif EarnedBadge.create(user_id: user.id, badge_id: badge.id)
       end
     end
   end
