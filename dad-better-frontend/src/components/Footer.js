1fac3867d5c128d92d6ee5be2fa4d996a455911b
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-14 11:03:03 -0500

    created

---
+++ dad-better-frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <div>
+            <p>Site created and owned by Chris Baum&nbsp;&nbsp;</p>
+        </div>
+    )
+}
+
+export default Footer
