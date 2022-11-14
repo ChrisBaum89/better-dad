a084b717d9177182547c9ab3c8d25e64832a222b
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-14 14:35:29 -0500

    removed empty space

--- dad-better-frontend/src/actions/userActions.js
+++ dad-better-frontend/src/actions/userActions.js
@@ -1,10 +1,9 @@
-
 export const fetchUsers = () => {
     debugger
     return (dispatch) => {
         dispatch({ type: 'LOADING_USERS' });
         fetch("http://localhost:3000/users")
             .then((response) => { return response.json() })
-            .then((responseJSON) => { dispatch({ type: "ADD_USERS", users: responseJSON })})
+            .then((responseJSON) => { dispatch({ type: "ADD_USERS", users: responseJSON }) })
     }
 }
