f08e1849a0c2ab57812ec4e7dca4c397cd0ff465
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-18 12:31:41 -0500

    added invalidLogin method

--- dad-better-frontend/src/components/LoginSignupForm.js
+++ dad-better-frontend/src/components/LoginSignupForm.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import { applyMiddleware } from 'redux';
+import "../css/App.css"
 
 
 function LoginSignupForm(props) {
-    const userState = useSelector((state) => state)
+    const currentState = useSelector((state) => state)
+    const currentUser = currentState.usersReducer.user[0]
     const dispatch = useDispatch()
 
     const handleOnSubmit = (event) => {
@@ -14,7 +17,7 @@ function LoginSignupForm(props) {
         const [username, password] = event.target
         sendLoginToServer(username.value, password.value)
     }
- 
+
     const sendLoginToServer = (username, password) => {
         fetch("http://localhost:3000/login", {
             method: "POST",
@@ -32,8 +35,20 @@ function LoginSignupForm(props) {
             .then((r) => r.json())
             .then((data) => {
                 localStorage.setItem("jwt", data.jwt)
-                dispatch({type: "UPDATE_USER", payload: data})
+                dispatch({ type: "UPDATE_USER", payload: data })
             })
+    }
+
+    const invalidLogin = () => {
+        if (currentUser !== undefined) {
+            if (currentState.usersReducer.user[0].message === "Invalid Login") {
+                return (
+                    <div className='invalid-login'>
+                        <div>Username or Password was not valid. Please try again.</div>
+                    </div>
+                )
+            }
+        }
     }
 
     return (
@@ -45,6 +60,7 @@ function LoginSignupForm(props) {
                 <Form.Group className="w-25" controlId="formBasicPassword">
                     <Form.Control type="password" size="sm" htmlSize="10" placeholder="Password" />
                 </Form.Group>
+                {invalidLogin()}
                 <Button variant="primary" type="submit">
                     Login
                 </Button>
