7330324b5b45a3af87babfbc27b506da90ff6013
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-18 10:36:22 -0500

    added several methods regarding user tasks

--- dad-better-backend/app/models/user.rb
+++ dad-better-backend/app/models/user.rb
@@ -13,9 +13,29 @@ class User < ApplicationRecord
 
   def calc_score
     score = 0
-    self.completed_tasks.each do |completed_task|
-      score = score + completed_task.task.value
+    completed_tasks.each do |completed_task|
+      score += completed_task.task.value
     end
-    return self.score = score
+    self.score = score
+  end
+
+  def number_assigned_tasks
+    assigned_tasks.length
+  end
+
+  def tasks_assigned_today?
+    current_date = DateTime.now.to_date
+    assigned_today = self.assigned_tasks.any? {|task| task.created_at.to_date == current_date}
+    completed_today = self.completed_tasks.any? {|task| task.created_at.to_date == current_date}
+    assigned_today || completed_today
+  end
+
+  def assign_todays_tasks
+    if not self.tasks_assigned_today?
+      10.times do
+        random_task_id = rand(1..Task.all.length)
+        AssignedTask.create(user_id: self.id, task_id: random_task_id)
+      end
+    end
   end
 end
