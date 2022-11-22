6dbb98374671b84daf7892aa3f4482659c95a16b
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-22 17:39:09 -0500

    functionality for editing user

--- dad-better-frontend/src/components/UserSettings.js
+++ dad-better-frontend/src/components/UserSettings.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
-function UserSettings(){
+function UserSettings() {
     const currentState = useSelector((state) => state)
     const currentUser = currentState.usersReducer.user[0].user
     const dispatch = useDispatch()
 
     const name = currentUser.data.attributes.name
     const email = currentUser.data.attributes.email
+    const [disableEdit, setDisableEdit] = useState(true)
+
+    const buttonDisplay = () => {
+        if (disableEdit === false) {
+            return <Button onClick={() => setDisableEdit(false)}>Submit</Button>
+        }
+        else{
+            return <Button onClick={() => setDisableEdit(false)}>Edit Settings and Password</Button>
+        }
+    }
 
     return (
         <div>
-            <h4>Name: {name}</h4>
-            <h4>Email: {email}</h4>
-            <Button>Edit Settings and Password</Button>
+            <Form.Group className="mb-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control placeholder={name} disabled={disableEdit} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control placeholder={email} disabled={disableEdit} />
+            </Form.Group>
+
+            {buttonDisplay()}
 
         </div>
     )
