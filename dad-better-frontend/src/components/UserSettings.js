a88d89fb8dc9a3410cf9e8f23f13b9154c602161
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-22 19:46:17 -0500

    updated to allow sending of updated data

--- dad-better-frontend/src/components/UserSettings.js
+++ dad-better-frontend/src/components/UserSettings.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
@@ -8,28 +8,64 @@ function UserSettings() {
     const currentUser = currentState.usersReducer.user[0].user
     const dispatch = useDispatch()
 
-    const name = currentUser.data.attributes.name
-    const email = currentUser.data.attributes.email
+    const userId = currentUser.data.id
     const [disableEdit, setDisableEdit] = useState(true)
+    const [name, setName] = useState(currentUser.data.attributes.name)
+    const [email, setEmail] = useState(currentUser.data.attributes.email)
+
+    const handleSubmit = (userId, name, email) => {
+        updateUserToServer(userId, name, email)
+        setDisableEdit(true)
+    }
 
     const buttonDisplay = () => {
         if (disableEdit === false) {
-            return <Button onClick={() => setDisableEdit(false)}>Submit</Button>
+            return <Button variant="primary" type="submit" onClick={() => handleSubmit(userId, name, email)}>Submit</Button>
         }
-        else{
+        else {
             return <Button onClick={() => setDisableEdit(false)}>Edit Settings and Password</Button>
         }
+    }
+
+    const updateUserToServer = (userId, name, email) => {
+        fetch("http://localhost:3000/updateuser", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                user: {
+                    user_id: userId,
+                    email: email,
+                    name: name,
+                    update_type: "update_user_settings",
+                },
+            }),
+        })
+            .then((r) => r.json())
+            .then((data) => {
+                localStorage.setItem("jwt", data.jwt)
+                dispatch({type: "UPDATE_USER", payload: data})
+             })
     }
 
     return (
         <div>
             <Form.Group className="mb-3">
                 <Form.Label>Name</Form.Label>
-                <Form.Control placeholder={name} disabled={disableEdit} />
+                <Form.Control 
+                placeholder={name} 
+                disabled={disableEdit}
+                onChange={e => setName(e.target.value)} 
+                />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Email</Form.Label>
-                <Form.Control placeholder={email} disabled={disableEdit} />
+                <Form.Control 
+                placeholder={email} 
+                disabled={disableEdit}
+                onChange={e => setEmail(e.target.value)} />
             </Form.Group>
 
             {buttonDisplay()}
