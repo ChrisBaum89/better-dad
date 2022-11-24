e140784af892fa2ace79a999070bac592f1f74aa
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-23 20:38:01 -0500

    updates to description (How it Works)

--- dad-better-frontend/src/components/Welcome.js
+++ dad-better-frontend/src/components/Welcome.js
@@ -3,8 +3,15 @@ import React from "react";
 function Welcome(){
     return (
         <div>
-        <h3>Welcome to Better Dad</h3>
-        <p> This application is to help dad's become better at "dadding" in a unique and fun way! If you are a returning user, please login.  If not please sign-up and let the fun begin!</p>
+        <h1>Welcome to Better Dad</h1>
+        <h5> This application is to help dad's become better at "dadding" in a unique and fun way! If you are a returning user, please login.  If not please sign-up and let the fun begin!</h5>
+        <br></br><br></br><br></br>
+        <h1>How It Works</h1>
+        <h5>You will be assigned daily tasks to complete. Some 
+            tasks are to tell dad jokes to others and some are to completed chores around the house.  Every day
+            new tasks will be loaded. You can gain achievements in the form of badges to show how great of a 
+            dad you are! The completion of a task is self-reporting, so though you could cheat, your honor as a man is on the line!
+            </h5>
         </div>
         )
 }
