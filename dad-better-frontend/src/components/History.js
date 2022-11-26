871732b658fc377cb0108021f5a2069bff2c3034
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 15:58:29 -0500

    updated to include display elements and pass props

--- dad-better-frontend/src/components/History.js
+++ dad-better-frontend/src/components/History.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import CompletedTaskCard from './CompletedTasksCard';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-function History(){
-    return(
-        <CompletedTaskCard/>
+function History(props) {
+    return (
+        <Offcanvas show={props.showHistory} onHide={props.handleHistoryClose} >
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>
+                    <div className="offcanvas-title">
+                        Completed Tasks
+                    </div>
+                </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <CompletedTaskCard />
+            </Offcanvas.Body>
+        </Offcanvas>
     )
 }
 
