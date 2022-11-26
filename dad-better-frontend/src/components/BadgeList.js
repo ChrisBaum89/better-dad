60922b2194e81b4b756faeb52b226140f113885b
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 16:14:00 -0500

    moved logic to BadgeContainer

--- dad-better-frontend/src/components/BadgeList.js
+++ dad-better-frontend/src/components/BadgeList.js
@@ -1,35 +1,18 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel'
-import { useSelector } from 'react-redux'
 import star_background from '../img/star_background.jpeg'
 import '../css/Badges.css'
 
-const BadgeList = () => {
+const BadgeList = (props) => {
 
-    const currentState = useSelector((state) => state)
-    const currentUser = currentState.usersReducer.user[0].user
-
-    const checkEarnedBadges = (badge) => {
-        return badge.type === "earned_badge"
-    }
-
-    const earnedBadges = (currentUser) => {
-        if (currentUser !== 0) {
-            return currentUser.included.filter(checkEarnedBadges)
-        }
-    }
-
-    const badges = earnedBadges(currentUser)
-
-
-    if (badges.length > 0) {
+    if (props.badges.length > 0) {
         return (
             <div class="badge-carousel">
                 <div class="badge-title">
                     <h2>Badges</h2>
                 </div>
                 <Carousel>
-                    {badges.map(badge => {
+                    {props.badges.map(badge => {
                         return (
                             <Carousel.Item interval={10000000}>
                                 <img
