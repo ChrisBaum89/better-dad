22214f76209b38f3ecc79befd2a10c69f507cdb3
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 16:47:09 -0500

    logic moved from Signup component

--- dad-better-frontend/src/containers/LoginSignupContainer.js
+++ dad-better-frontend/src/containers/LoginSignupContainer.js
@@ -2,25 +2,55 @@ import React, {useState} from 'react';
 import LoginSignupForm from '../components/LoginSignupForm';
 import Signup from '../components/Signup';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useDispatch } from 'react-redux';
 
 function LoginSignupContainer(props) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const dispatch = useDispatch()
+
+
+    const handleOnSubmit = (event) => {
+        event.preventDefault()
+        const [username, password, name, email] = event.target
+        createUser(username.value, password.value, name.value, email.value)
+    }
+
+    const createUser = (username, password, name, email) => {
+        fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                user: {
+                    username: username,
+                    password: password,
+                    email: email,
+                    name: name,
+                },
+            }),
+        })
+            .then((r) => r.json())
+            .then((data) => {
+                localStorage.setItem("jwt", data.jwt)
+                dispatch({ type: "LOGIN_USER", payload: data })
+            })
+    }
     
     return (
         <div>
             <LoginSignupForm handleShow={handleShow}/>
 
-            <Offcanvas show={show} onHide={handleClose}>
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Signup</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <Signup />
-                </Offcanvas.Body>
-            </Offcanvas>
+           <Signup
+            handleOnSubmit={handleOnSubmit}
+            handleClose={handleClose}
+            show={show}
+           />
         </div>
     )
 
