0f566e85c9d90492c87d15c277a13ebbcf755429
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 16:39:34 -0500

    moved majority of logic to TaskContainer

--- dad-better-frontend/src/components/TaskList.js
+++ dad-better-frontend/src/components/TaskList.js
@@ -3,73 +3,46 @@ import Carousel from 'react-bootstrap/Carousel'
 import task_background from '../img/task_background.jpeg'
 import '../css/Tasks.css'
 import Button from 'react-bootstrap/Button'
-import { useSelector, useDispatch } from 'react-redux';
 
 function TaskList(props) {
-    const currentState = useSelector((state) => state)
-    const currentUser = currentState.usersReducer.user[0].user
-    const assignedTasks = props.userAssignedTasks
-    const jwtToken = localStorage.jwt
-
-    const dispatch = useDispatch()
-
-    const updateUserToServer = (user, taskId) => {
-        fetch("http://localhost:3000/updateuser", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `bearer ${jwtToken}`,
-                Accept: "application/json",
-            },
-            body: JSON.stringify({
-                user: {
-                    user_id: user.data.id,
-                    task_id: taskId,
-                    update_type: "task_completed"
-                },
-            }),
-        })
-            .then((r) => r.json())
-            .then((data) => {
-                dispatch({ type: "UPDATE_USER", payload: data })
-            })
-    }
-
-
-    const handleClick = (event, user = currentUser) => {
-        const taskId = event.target.attributes.taskid.value
-        updateUserToServer(user, taskId)
-    }
 
     const checkIfAssignedTasks = (tasks) => {
         if (tasks.length > 0) {
             return (
-                <Carousel>
-                    {tasks.map(task => {
-                        return (
-                            <Carousel.Item interval={10000000}>
-                                <img
-                                    className="d-block w-100"
-                                    src={task_background}
-                                    alt="First slide"
-                                />
-                                <Carousel.Caption>
-                                    <div class="task-carousel-caption">
-                                        <h6>{task.attributes.task.description}</h6>
-                                        <p>Points: {task.attributes.task.value}</p>
-                                        <Button variant="primary" taskid={task.attributes.task.id} taskvalue={task.attributes.task.value} onClick={handleClick}>
-                                            Complete
-                                        </Button>
-                                    </div>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        )
-                    })}
-                </Carousel>
+                <div>
+                    <div class="task-title">
+                        <h2>Tasks</h2>
+                    </div>
+                    <Carousel>
+                        {tasks.map(task => {
+                            return (
+                                <Carousel.Item interval={10000000}>
+                                    <img
+                                        className="d-block w-100"
+                                        src={task_background}
+                                        alt="First slide"
+                                    />
+                                    <Carousel.Caption>
+                                        <div class="task-carousel-caption">
+                                            <h6>{task.attributes.task.description}</h6>
+                                            <p>Points: {task.attributes.task.value}</p>
+                                            <Button variant="primary" taskid={task.attributes.task.id} taskvalue={task.attributes.task.value} onClick={props.handleClick}>
+                                                Complete
+                                            </Button>
+                                        </div>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            )
+                        })}
+                    </Carousel>
+                </div>
             )
         }
         else {
-            return (
+            return (<div>
+                <div class="task-title">
+                    <h2>Tasks</h2>
+                </div>
                 <Carousel>
                     <Carousel.Item interval={10000000}>
                         <img
@@ -84,13 +57,14 @@ function TaskList(props) {
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
+            </div>
             )
         }
     }
 
     return (
         <div class="task-carousel">
-            {checkIfAssignedTasks(assignedTasks)}
+            {checkIfAssignedTasks(props.tasks)}
         </div>
     )
 
