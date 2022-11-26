25ce912d6c05b4ca45d26bf95a27088a0f8e998e
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 16:15:31 -0500

    changes in progress during testing/development

--- dad-better-frontend/src/components/UserCard.js
+++ dad-better-frontend/src/components/UserCard.js
@@ -4,18 +4,20 @@ import '../css/UserCard.css'
 
 const UserCard = (props) => {
     return (
-        <div className="col d-flex justify-content-center">
-            <Card style={{ width: '18rem' }}>
-                <Card.Img className="user-image" variant="top" src="https://c8.alamy.com/comp/2J7983E/dad-pixel-art-vector-illustration-father-day-image-or-clip-art-2J7983E.jpg" />
-                <Card.Body>
-                    <Card.Text>
-                        Username: {props.username}
-                    </Card.Text>
-                    <Card.Text>
-                        Score:  {props.score}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+        <div className="user-card">
+            <div className="col d-flex justify-content-center">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img className="user-image" variant="top" src="https://c8.alamy.com/comp/2J7983E/dad-pixel-art-vector-illustration-father-day-image-or-clip-art-2J7983E.jpg" />
+                    <Card.Body>
+                        <Card.Text>
+                            Username: {props.username}
+                        </Card.Text>
+                        <Card.Text>
+                            Score:  {props.score}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     )
 }
