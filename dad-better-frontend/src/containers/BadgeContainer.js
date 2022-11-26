ce2d61d278e99f969afbbd27dfc2037d24013a91
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 16:14:31 -0500

    logic moved from BadgeList to BadgeContainer

--- dad-better-frontend/src/containers/BadgeContainer.js
+++ dad-better-frontend/src/containers/BadgeContainer.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import BadgeList from "../components/BadgeList.js";
+import React from "react";
+import { useSelector } from 'react-redux'
+import '../css/Badges.css'
+import BadgeList from "../components/BadgeList";
 
-function BadgeContainer(props) {
-    return (
+function BadgeContainer () {
+
+    const currentState = useSelector((state) => state)
+    const currentUser = currentState.usersReducer.user[0].user
+
+    const checkEarnedBadges = (badge) => {
+        return badge.type === "earned_badge"
+    }
+
+    const earnedBadges = (currentUser) => {
+        if (currentUser !== 0) {
+            return currentUser.included.filter(checkEarnedBadges)
+        }
+    }
+
+    const badges = earnedBadges(currentUser)
+debugger
+    return(
         <div>
-            <BadgeList />
+            <BadgeList badges={badges}/>
         </div>
     )
+
 }
 
 export default BadgeContainer
