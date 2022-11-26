60fab6403e4476f6c2c83a9a925aab485ebb55f7
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 15:06:44 -0500

    file created

---
+++ dad-better-frontend/src/components/Quote.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Quote = (props) => {
+    debugger
+    return (
+        <div>
+            {props.quote.description}
+        </div>
+    )
+}
+
+export default Quote
