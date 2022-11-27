a2fddb1f372f61ea83d9851e8d2ff86582434215
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-27 15:23:04 -0500

    added file

---
+++ dad-better-frontend/src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AboutLinks from "./AboutLinks";
+
+
+function About() {
+    return (
+        <div>
+            <AboutLinks />
+            <h1>How It Works</h1>
+            <h5>You will be assigned daily tasks to complete. Some
+                tasks are to tell dad jokes to others and some are to completed chores around the house.  Every day
+                new tasks will be loaded. You can gain achievements in the form of badges to show how great of a
+                dad you are! The completion of a task is self-reporting, so though you could cheat, your honor as a man is on the line!
+            </h5>
+        </div>
+    )
+}
+export default About
