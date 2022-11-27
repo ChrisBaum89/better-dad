6d2c442eb2a4b8bac858247c7bdc6a8d1feff8b7
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-27 15:40:48 -0500

    added WelcomeLinks

--- dad-better-frontend/src/containers/WelcomeContainer.js
+++ dad-better-frontend/src/containers/WelcomeContainer.js
@@ -2,10 +2,13 @@ import React from 'react';
 import LoginSignupContainer from './LoginSignupContainer';
 import Welcome from '../components/Welcome';
 import Logo from '../components/Logo';
+import AboutLink from '../components/WelcomeLink';
+import WelcomeLinks from '../components/WelcomeLink';
 
 function WelcomeContainer() {
     return (
         <div>
+            <WelcomeLinks />
             <div className='logo-signin-form'>
                 <div className='welcome-logo-spacing'><p>&nbsp;</p></div>
                 <div className="welcome-logo">
