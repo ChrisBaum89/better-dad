2f5f4ce35fbfc366b5b0d73247c76b3aab181abd
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-27 15:41:24 -0500

    deleted quotesReducer and tasksReducer

--- dad-better-frontend/src/reducers/indexReducer.js
+++ dad-better-frontend/src/reducers/indexReducer.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
-import quotesReducer from './quotesReducer';
 import usersReducer from './usersReducer'
-import tasksReducer from './tasksReducer'
 
 export default combineReducers({
   usersReducer,
-  quotesReducer,
-  tasksReducer,
 });
