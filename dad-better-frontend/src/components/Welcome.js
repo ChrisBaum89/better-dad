b5d28cb2bad3c27d1d53a98d94595cbc0d1089d3
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-27 12:16:36 -0500

    add className and extra spacing

--- dad-better-frontend/src/components/Welcome.js
+++ dad-better-frontend/src/components/Welcome.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../css/App.css"
 
 function Welcome(){
     return (
-        <div>
+        <div className="welcome-div">
         <h1>Welcome to Better Dad</h1>
         <h5> This application is to help dad's become better at "dadding" in a unique and fun way! If you are a returning user, please login.  If not please sign-up and let the fun begin!</h5>
         <br></br><br></br><br></br>
@@ -12,6 +13,9 @@ function Welcome(){
             new tasks will be loaded. You can gain achievements in the form of badges to show how great of a 
             dad you are! The completion of a task is self-reporting, so though you could cheat, your honor as a man is on the line!
             </h5>
+
+            <br></br>
+            <br></br>
         </div>
         )
 }
