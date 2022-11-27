7ff6b0cd465cd4c3c1e129375fbde55bdbfc9d14
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-26 22:20:56 -0500

    added divs to allow for improved layout

--- dad-better-frontend/src/containers/WelcomeContainer.js
+++ dad-better-frontend/src/containers/WelcomeContainer.js
@@ -6,8 +6,13 @@ import Logo from '../components/Logo';
 function WelcomeContainer() {
     return (
         <div>
-            <Logo />
-            <LoginSignupContainer />
+            <div className='logo-signin-form'>
+                <div className='welcome-logo-spacing'><p>&nbsp;</p></div>
+                <div className="welcome-logo">
+                    <Logo />
+                </div>
+                <LoginSignupContainer />
+            </div>
             <br></br>
             <br></br>
             <Welcome />
