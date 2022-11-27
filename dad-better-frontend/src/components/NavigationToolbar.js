447e51ee6597b7c24522e74b18ca9ef27e0692c1
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-27 15:39:36 -0500

    changed User Settings to Settings

--- dad-better-frontend/src/components/NavigationToolbar.js
+++ dad-better-frontend/src/components/NavigationToolbar.js
@@ -3,14 +3,14 @@ import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
-function NavigationToolbar (props) {
+function NavigationToolbar(props) {
     return (
         <div>
             <ButtonToolbar aria-label="Toolbar with button groups">
                 <ButtonGroup className="me-2" aria-label="First group">
                     <Button onClick={props.handleHistory}>History</Button>
                     <Button onClick={props.handleFavorites}>Favorites</Button>
-                    <Button onClick={props.handleSettings}>User Settings</Button>
+                    <Button onClick={props.handleSettings}>Settings</Button>
                     <Button onClick={props.handleLogout}>Logout</Button>
                 </ButtonGroup>
             </ButtonToolbar>
