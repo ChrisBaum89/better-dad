107d7f84e50dec5f4b3c6189900a41f3cf466f0b
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-27 15:41:09 -0500

    deleted file

--- dad-better-frontend/src/reducers/tasksReducer.js
+++
@@ -1,9 +0,0 @@
-const tasksReducer = (state = { tasks: [], requesting: false }, action) => {
-    switch (action.type) {
-
-        default:
-            return state
-    }
-}
-
-export default tasksReducer
