19a92b42280243af9582ce927d0ee07cbf0cb9b1
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-28 11:38:17 -0500

    removed break and used css for spacing

--- dad-better-frontend/src/components/Welcome.js
+++ dad-better-frontend/src/components/Welcome.js
@@ -4,9 +4,10 @@ import "../css/App.css"
 function Welcome() {
     return (
         <div className="welcome-div">
+            <div className="welcome-content-div">
             <h1>Welcome to Better Dad</h1>
             <h5> This application is to help dad's become better at "dadding" in a unique and fun way! If you are a returning user, please login.  If not please sign-up and let the fun begin!</h5>
-            <br></br>
+            </div>
         </div>
     )
 }
