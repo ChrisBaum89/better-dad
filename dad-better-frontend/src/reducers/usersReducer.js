e2f4fa07389130ab472aa448851fd140fe95e983
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-28 10:29:05 -0500

    added INITIALIZE and CLEAR_MESSAGE

--- dad-better-frontend/src/reducers/usersReducer.js
+++ dad-better-frontend/src/reducers/usersReducer.js
@@ -1,8 +1,9 @@
 const usersReducer = (state = { user: [], loading: false }, action) => {
     switch (action.type) {
         
-        // case "SET_USER":
-        //     return { ...state, user: [action.payload] }
+        case "INITIALIZE":
+            const userArray = {message: "NO USER"}
+            return { ...state, user: [userArray] }
 
         case "UPDATE_USER":
             return {...state, user: [action.payload]}
@@ -12,6 +13,10 @@ const usersReducer = (state = { user: [], loading: false }, action) => {
 
         case "LOGOUT":
             return { ...state, user: [] }
+
+        case "CLEAR_MESSAGE":
+            state.user[0].message = ""
+            return {...state}
 
         default:
             return state
