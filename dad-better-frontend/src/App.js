d62ce01e8af9240e56e4656535cc5956afba51a7
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-28 10:28:19 -0500

    added dispatch to loggedin === undefined

--- dad-better-frontend/src/App.js
+++ dad-better-frontend/src/App.js
@@ -3,7 +3,7 @@ import './css/App.css';
 import ProfileContainer from "./containers/ProfileContainer";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import WelcomeContainer from './containers/WelcomeContainer';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import Footer from './components/Footer';
 import About from './components/About';
@@ -11,10 +11,12 @@ import About from './components/About';
 function App() {
 
   const currentState = useSelector((state) => state)
+  const dispatch = useDispatch()
 
   const loggedin = () => {
     const user = currentState.usersReducer.user[0]
     if (user === undefined) {
+      dispatch({type: "INITIALIZE"})
       return false
     }
     else if (localStorage.jwt === 'undefined') {
