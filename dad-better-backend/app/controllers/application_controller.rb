f8e6058a0128b658c750508cd87d81bc9cb4ef83
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-29 10:10:26 -0500

    fixed ends that didn't line up

--- dad-better-backend/app/controllers/application_controller.rb
+++ dad-better-backend/app/controllers/application_controller.rb
@@ -54,36 +54,36 @@ class ApplicationController < ActionController::API
            },
            status: :created
   end
-end
 
-def render_invalid_login_json
-  render json: {
-    message: 'Invalid Login'
-  },
-  status: :unauthorized
-end
-
-def update_badge(user)
-  applicable_badges = Badge.all.select { |badge| user.score >= badge.score_threshold }
-  applicable_badges.each do |badge|
-    EarnedBadge.create(user_id: user.id, badge_id: badge.id) if user.badges.exclude?(badge)
+  def render_invalid_login_json
+    render json: {
+      message: 'Invalid Login'
+    },
+    status: :unauthorized
   end
-end
 
-def quote_of_day
-  activeQuote = Quote.all.select { |quote| quote.active == true }
-  activeQuote[0]
-end
+  def update_badge(user)
+    applicable_badges = Badge.all.select { |badge| user.score >= badge.score_threshold }
+    applicable_badges.each do |badge|
+      EarnedBadge.create(user_id: user.id, badge_id: badge.id) if user.badges.exclude?(badge)
+    end
+  end
 
-def setup_authorized_user(user, token)
-  if user
-    user.assign_daily_tasks
-    user.calc_score
-    update_badge(user)
-    assign_quote_of_day
-    token = encode_token({ user_id: @user.id })
-    render_user_json(user, token)
-  else
-    render_invalid_login_json
+  def quote_of_day
+    activeQuote = Quote.all.select { |quote| quote.active == true }
+    activeQuote[0]
+  end
+
+  def setup_authorized_user(user, token)
+    if user
+      user.assign_daily_tasks
+      user.calc_score
+      update_badge(user)
+      assign_quote_of_day
+      token = encode_token({ user_id: @user.id })
+      render_user_json(user, token)
+    else
+      render_invalid_login_json
+    end
   end
 end
