339ed35d9491c7a91c6109f9c86d662cffd3346f
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-29 09:45:25 -0500

    deleted decoded_...jwt_login, added setup_auth...

--- dad-better-backend/app/controllers/application_controller.rb
+++ dad-better-backend/app/controllers/application_controller.rb
@@ -25,15 +25,6 @@ class ApplicationController < ActionController::API
     end
   end
 
-  def decoded_token_jwt_login(token)
-    begin
-      JWT.decode(token, 'the_s3cr3t', true, algorithm: 'HS256')
-    # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
-    rescue JWT::DecodeError
-      nil
-    end
-  end
-
   def logged_in_user
     if decoded_token
       # decoded_token=> [{"user_id"=>2}, {"alg"=>"HS256"}]
@@ -59,23 +50,47 @@ class ApplicationController < ActionController::API
              user: UserSerializer.new(user, options),
              jwt: token,
              message: 'Valid Login',
-             quote: quote_of_day(),
+             quote: quote_of_day
            },
            status: :created
   end
-
 end
 
 def update_badge(user)
   applicable_badges = Badge.all.select { |badge| user.score >= badge.score_threshold }
   applicable_badges.each do |badge|
-    if user.badges.exclude?(badge)
-      EarnedBadge.create(user_id: user.id, badge_id: badge.id)
-    end
+    EarnedBadge.create(user_id: user.id, badge_id: badge.id) if user.badges.exclude?(badge)
   end
 end
 
-def quote_of_day()
-  activeQuote = Quote.all.select{|quote| quote.active == true}
-  return activeQuote[0]
+def quote_of_day
+  activeQuote = Quote.all.select { |quote| quote.active == true }
+  activeQuote[0]
+end
+
+def setup_authorized_user(user, token)
+  if user
+    user.assign_daily_tasks
+    user.calc_score
+    update_badge(user)
+    assign_quote_of_day()
+    token = encode_token({ user_id: @user.id })
+
+    options = {
+      include: %i[assigned_tasks completed_tasks earned_badges]
+    }
+
+    render json: {
+             user: UserSerializer.new(user, options),
+             message: 'Valid Login',
+             quote: quote_of_day(),
+             jwt: token,
+           },
+           status: :accepted
+  else
+    render json: {
+             message: 'Invalid Login'
+           },
+           status: :unauthorized
+  end
 end
