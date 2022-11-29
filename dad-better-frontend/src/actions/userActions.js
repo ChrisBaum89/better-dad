ce8ab30e3996a08252c39d151d9891acca149d25
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-29 17:12:24 -0500

    added fetchUser

--- dad-better-frontend/src/actions/userActions.js
+++ dad-better-frontend/src/actions/userActions.js
@@ -1,9 +1,23 @@
-export const fetchUsers = () => {
-    debugger
+export const fetchUser = (username, password) => {
     return (dispatch) => {
-        dispatch({ type: 'LOADING_USERS' });
-        fetch("http://localhost:3000/users")
-            .then((response) => { return response.json() })
-            .then((responseJSON) => { dispatch({ type: "ADD_USERS", users: responseJSON }) })
+        dispatch({ type: "START_LOGIN_USER_REQUEST" });
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                user: {
+                    username: username,
+                    password: password,
+                    message: "new user login"
+                },
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                dispatch({ type: "LOGIN_USER", payload: data })
+            })
     }
 }
