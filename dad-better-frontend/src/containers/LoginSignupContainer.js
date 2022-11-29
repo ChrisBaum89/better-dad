6227b815c5f627b71e107a009744c4b9458137fb
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-29 15:26:33 -0500

    moved logic from component to container

--- dad-better-frontend/src/containers/LoginSignupContainer.js
+++ dad-better-frontend/src/containers/LoginSignupContainer.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import LoginSignupForm from '../components/LoginSignupForm';
+import LoginSignupForm from '../components/LoginSignup';
 import Signup from '../components/Signup';
 import { useDispatch, useSelector } from 'react-redux';
 import "../css/App.css"
@@ -9,6 +9,7 @@ function LoginSignupContainer() {
 
     const dispatch = useDispatch()
     const currentState = useSelector((state) => state)
+    const currentUser = currentState.usersReducer.user[0]
     const user = currentState.usersReducer.user[0]
 
     const messaging = (user) => {
@@ -34,6 +35,12 @@ function LoginSignupContainer() {
         createUser(username.value, password.value, name.value, email.value)
     }
 
+    const handleLoginOnSubmit = (event) => {
+        event.preventDefault()
+        const [username, password] = event.target
+        sendLoginToServer(username.value, password.value)
+    }
+
     const createUser = (username, password, name, email) => {
         fetch("http://localhost:3000/users", {
             method: "POST",
@@ -57,9 +64,47 @@ function LoginSignupContainer() {
             })
     }
 
+    const sendLoginToServer = (username, password) => {
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                user: {
+                    username: username,
+                    password: password,
+                    message: "new user login"
+                },
+            }),
+        })
+            .then((r) => r.json())
+            .then((data) => {
+                localStorage.setItem("jwt", data.jwt)
+                dispatch({ type: "LOGIN_USER", payload: data })
+            })
+    }
+
+    const invalidLogin = () => {
+        if (currentUser !== undefined) {
+            if (currentState.usersReducer.user[0].message === "Invalid Login") {
+                return (
+                    <div className='invalid-login'>
+                        <div>Username or Password was not valid. Please try again.</div>
+                    </div>
+                )
+            }
+        }
+    }
+
     return (
         <div className='login-signin-form'>
-            <LoginSignupForm handleShow={handleShow}/>
+            <LoginSignupForm 
+            handleShow={handleShow}
+            handleOnSubmit={handleLoginOnSubmit}
+            invalidLogin={invalidLogin}
+            />
 
            <Signup
             handleOnSubmit={handleSignupOnSubmit}
