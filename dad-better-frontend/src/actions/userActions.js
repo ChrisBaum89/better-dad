a219e93fb1922726fab5ccd972a6e38ba2b54cd5
Chris Baum <Christopher.Baum@valpo.edu>
2022-11-30 11:27:17 -0500

    working on simplifying fetch functions

--- dad-better-frontend/src/actions/userActions.js
+++ dad-better-frontend/src/actions/userActions.js
@@ -1,23 +1,33 @@
-export const fetchUser = (username, password) => {
+export const fetchUser = (
+    userId = null,
+    username = '',
+    password = '',
+    email = '',
+    name = '',
+    existingPassword = '',
+    newPassword = '',
+    message = '',
+    updateType = '',
+    dispatchType = '',
+    fetchUrl = ''
+) => {
     return (dispatch) => {
         dispatch({ type: "START_USER_REQUEST" });
-        fetch("http://localhost:3000/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify({
-                user: {
-                    username: username,
-                    password: password,
-                    message: "new user login"
-                },
-            }),
-        }) 
-        .then((response) => response.json())
+        fetch(fetchUrl,
+            jsonUserCommon(
+                userId,
+                username,
+                password,
+                email,
+                name,
+                existingPassword,
+                newPassword,
+                message,
+                updateType,
+                ))
+            .then((response) => response.json())
             .then((data) => {
-                dispatch({ type: "LOGIN_USER", payload: data })
+                dispatch({ type: dispatchType, payload: data })
             })
     }
 }
@@ -32,14 +42,7 @@ export const fetchUserUpdate = (userId, name, email) => {
                 Authorization: `bearer ${localStorage.jwt}`,
                 Accept: "application/json",
             },
-            body: JSON.stringify({
-                user: {
-                    user_id: userId,
-                    email: email,
-                    name: name,
-                    update_type: "update_user_settings",
-                },
-            }),
+            body: JSON.stringify(jsonUserUpdate(userId, name, email)),
         })
             .then((r) => r.json())
             .then((data) => {
@@ -58,14 +61,7 @@ export const fetchPasswordUpdate = (userId, existingPassword, newPassword) => {
                 Authorization: `bearer ${localStorage.jwt}`,
                 Accept: "application/json",
             },
-            body: JSON.stringify({
-                user: {
-                    user_id: userId,
-                    existing_password: existingPassword,
-                    new_password: newPassword,
-                    update_type: "update_password",
-                },
-            }),
+            body: JSON.stringify(jsonPasswordUpdate(userId, existingPassword, newPassword)),
         })
             .then((r) => r.json())
             .then((data) => {
@@ -73,3 +69,96 @@ export const fetchPasswordUpdate = (userId, existingPassword, newPassword) => {
             })
     }
 }
+
+export const jsonUserCommon = (
+    userId = 0,
+    username = '',
+    password = '',
+    email = '',
+    name = '',
+    existingPassword = '',
+    newPassword = '',
+    message = '',
+    updateType = '',
+) => {
+    return (
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `bearer ${localStorage.jwt}`,
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                user: {
+                    user_id: userId,
+                    username: username,
+                    password: password,
+                    email: email,
+                    name: name,
+                    existing_password: existingPassword,
+                    new_password: newPassword,
+                },
+                message: message,
+                update_type: updateType
+            }),
+        })
+}
+
+export const jsonUser = (username, password) => {
+    return (
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `bearer ${localStorage.jwt}`,
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                user: {
+                    username: username,
+                    password: password,
+                    message: "new user login"
+                },
+            }),
+        })
+}
+
+export const jsonCreateUser = (username, password, email, name) => {
+    return (
+        {
+            user: {
+                username: username,
+                password: password,
+                email: email,
+                name: name,
+                update_type: 'create user'
+            }
+        }
+    )
+}
+
+export const jsonPasswordUpdate = (userId, existingPassword, newPassword) => {
+    return (
+        {
+            user: {
+                user_id: userId,
+                existing_password: existingPassword,
+                new_password: newPassword,
+                update_type: "update_password",
+            }
+        })
+}
+
+export const jsonUserUpdate = (userId, name, email) => {
+    return (
+        {
+            user: {
+                user_id: userId,
+                email: email,
+                name: name,
+                update_type: "update_user_settings",
+            }
+        })
+}
+
