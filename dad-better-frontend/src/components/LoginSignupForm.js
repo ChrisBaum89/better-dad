74151197f8d063d4ad4806888e2654f3c1a926f8
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-02 17:30:30 -0500

    formatting changes and div className assign

--- dad-better-frontend/src/components/LoginSignupForm.js
+++ dad-better-frontend/src/components/LoginSignupForm.js
@@ -5,23 +5,27 @@ import "../css/App.css"
 
 
 function LoginSignupForm(props) {
-    
+
     return (
         <div className='login-form'>
-            <Form onSubmit={props.handleOnSubmit}>
-                <Form.Group className="w-100" controlId="formBasicEmail">
-                    <Form.Control required type="username" size="sm" placeholder="Username" />
-                </Form.Group>
-                <Form.Group className="w-100" controlId="formBasicPassword">
-                    <Form.Control required type="password" size="sm" htmlSize="10" placeholder="Password" />
-                </Form.Group>
-                {props.invalidLogin()}
-                <br></br>
-                <Button variant="custom" type="submit" style={{
-                    color: "#fff3e1", background: "black"}}>Login</Button>
-                <Button variant="custom" onClick={props.handleShow} style={{
-                    color: "#fff3e1", background: "black"}}>Signup</Button>
-            </Form>
+                <Form onSubmit={props.handleOnSubmit}>
+                    <div className="login-form-content">
+                    <Form.Group className="w-100" controlId="formBasicEmail">
+                        <Form.Control required type="username" size="sm" placeholder="Username" />
+                    </Form.Group>
+                    <Form.Group className="w-100" controlId="formBasicPassword">
+                        <Form.Control required type="password" size="sm" htmlSize="10" placeholder="Password" />
+                    </Form.Group>
+                    </div>
+                    {props.invalidLogin()}
+                    <br></br>
+                    <Button variant="custom" type="submit" style={{
+                        color: "#fff3e1", background: "black"
+                    }}>Login</Button>
+                    <Button variant="custom" onClick={props.handleShow} style={{
+                        color: "#fff3e1", background: "black"
+                    }}>Signup</Button>
+                </Form>
         </div>
     )
 
