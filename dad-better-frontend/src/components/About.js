d60e18cba36a3ae3d103b089695254e33490e4d2
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-02 12:05:18 -0500

    deleted

--- dad-better-frontend/src/components/About.js
+++ dad-better-frontend/src/components/About.js
@@ -13,23 +13,14 @@ function About() {
             </div>
             <div className='about-div'>
                 <div className='about-content'>
-                    <h1>How It Works</h1>
-                    <h5>You will be assigned daily tasks to complete. Some
-                        tasks are to share the joy of dad jokes with others and some are to completed chores around the house.  Every day
-                        new tasks will be loaded. You can gain achievements in the form of badges to show how great of a
-                        dad you are! The completion of a task is self-reporting, so though you could cheat, your honor as a man is on the line!
-                    </h5>
-                    <br></br>
-                    <h5>The badges you earn are typically in the theme of the funny stereotype of the "old school dad", which may or may not apply to you. These are just for laughs
-                        and are no way representative of how you should be. You do you and be the dad that you think you should be!
-                    </h5>
-                    <br></br>
-                    <h1>About</h1>
-                    <h5>This application was created to help dads become more productive and enjoy embracing being a dad.
-                        Being a dad is amazing and enriches your life!  But just like many things worth doing in life
-                        it can lead to moments of boredom, frustration, and depression as you try to navigate family life and
-                        work life.  This project was inspired by a TED talk that I listened to about how Jane McGonigal fought
-                        severe depression by treating her life like a game to get little wins and progress through challenging times and emotions.
+                <h1>About</h1>
+                    <h5><p>Being a dad is amazing and enriches your life!  But just like many things worth doing in life, you do it for a long time, day after day.
+                        This can lead to moments of boredom, frustration, and depression as you try to navigate family life and
+                        work life. This can be a difficult feeling for dads to feel as it can sometimes feel like society looks down upon these feelings
+                        and we should be "putting our kids first". I will tell you from personal experience and those of friends that you are a better parent
+                        when you feel like you are mentally in a good place.</p>
+                        <p>This application was created to help dads become more productive and enjoy embracing being a dad. This project was inspired by a TED talk that I listened to about how Jane McGonigal fought
+                        severe depression by treating her life like a game to get little wins and progress through challenging times and emotions.</p>
                     </h5>
                     <div className="video-outer-div">
                         <div className="video-inner-div">
@@ -46,9 +37,21 @@ function About() {
                             </iframe>
                         </div>
                     </div>
+                    <br></br>
+                    <h1>How It Works</h1>
+                    <h5>You will be assigned daily tasks to complete. Some
+                        tasks are to share the joy of dad jokes with others and some are to completed chores around the house.  Every day
+                        new tasks will be loaded. You can gain achievements in the form of badges to show how great of a
+                        dad you are! The completion of a task is self-reporting, so though you could cheat, your honor as a man is on the line!
+                    </h5>
+                    <br></br>
+                    <h5>The badges you earn are typically in the theme of the funny stereotype of the "old school dad", which may or may not apply to you. These are just for laughs
+                        and are no way representative of how you should be. You do you and be the dad that you think you should be!
+                    </h5>
+                    <br></br>
                     <h5>
-                        This project was created using React (v18.2.0), Redux (v4.2.0), Thunk (v2.4.1), and fetches data from a Ruby on Rails (v6.1.6) API.
-                        Formatting was done using HTML, CSS, and React-Bootstrap.
+                        This project was created using React (v18.2.0). Redux (v4.2.0) and Thunk (v2.4.1) are utilized for state management. The React application fetches data from a Ruby on Rails API (v6.1.6).
+                        Pictures used are from <a href="https://www.pexels.com/">Pexels</a>.
                     </h5>
                 </div >
             </div>
