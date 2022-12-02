ed47cd711b733c438ba653d6787af2435ceda40c
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-02 14:26:09 -0500

    added divs for better formatting

--- dad-better-frontend/src/components/Quote.js
+++ dad-better-frontend/src/components/Quote.js
@@ -3,8 +3,12 @@ import '../css/App.css'
 
 const Quote = (props) => {
     return (
-        <div className='quote-div' float="right">
-            "{props.quote.description}" - {props.quote.author}
+        <div className='quote-div'>
+            <div className="quote-content-div">
+                <div className="quote-text">
+                <p>"{props.quote.description}" - {props.quote.author}</p>
+                </div>
+            </div>
         </div>
     )
 }
