86215d69a58badea387dddb582c6236819d7e3d7
Chris Baum <Christopher.Baum@valpo.edu>
2022-12-02 15:57:10 -0500

    modified imported background and formatting

--- dad-better-frontend/src/components/BadgeList.js
+++ dad-better-frontend/src/components/BadgeList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel'
-import star_background from '../img/star_background.jpeg'
+import badge_background from '../img/badge_background.jpg'
 import '../css/Badges.css'
 
 const BadgeList = (props) => {
@@ -8,20 +8,23 @@ const BadgeList = (props) => {
     if (props.badges.length > 0) {
         return (
             <div className="badge-carousel">
-                <div className="badge-title">
-                    <h2>Badges Earned</h2>
-                </div>
-                <div className="badge-title-content-divider"></div>
                 <Carousel>
                     {props.badges.map(badge => {
                         return (
                             <Carousel.Item interval={10000000} key={`badge-${badge.id}`}>
+
                                 <img
                                     className="d-block w-100"
-                                    src={star_background}
+                                    src={badge_background}
                                     alt="First slide"
                                 />
+
                                 <Carousel.Caption>
+                                    <div className="badge-title">
+                                        <div className="badge-title-content">
+                                            <h2>Badges Earned</h2>
+                                        </div>
+                                    </div>
                                     <div className="badge-carousel-caption">
                                         <img class='badge-image' src={badge.attributes.badge.image} alt=''></img>
                                         <br></br><br></br>
@@ -38,15 +41,16 @@ const BadgeList = (props) => {
     } else {
         return (
             <div className="badge-carousel">
-                <div className="badge-title">
-                    <h2>Badges Earned</h2>
-                </div>
+
                 <div className="badge-carousel-badges">
                     <Carousel>
+                        <div className="badge-title">
+                            <h2>Badges Earned</h2>
+                        </div>
                         <Carousel.Item interval={10000000}>
                             <img
                                 className="d-block w-100"
-                                src={star_background}
+                                src={badge_background}
                                 alt="First slide"
                             />
                             <Carousel.Caption>
